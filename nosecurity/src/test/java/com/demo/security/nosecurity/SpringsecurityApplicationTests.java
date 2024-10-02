a67570b9f9d4776144de8af42c9128eac5a64b98
Shridhar Pralhad Vipradas <shridharvipradas@gmail.com>
2024-10-02 20:41:47 +0530

    Initial commit

---
+++ nosecurity/src/test/java/com/demo/security/nosecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.security.nosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
